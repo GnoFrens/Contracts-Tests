fa4d4348e08d25db8dae5d529bcb26d947218b4f
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-23 11:55:16 +0900

    updtaed packages

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,8 @@ require('babel-register');
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 require('dotenv').config();
 const privateKey = process.env.WALLET_PRIVATE_KEY;
-
+const gnosisscanAPIKey = process.env.BLOCK_SCOUT_API_KEY;
+const etherScanPrivateKey = process.env.ETHERSCAN_PRIVATE_KEY;
 module.exports = {
   networks: {
     live: {
@@ -16,6 +17,13 @@ module.exports = {
       network_id: 1,        // Ethereum public network,
       */
     },
+    chiado: {
+      provider: function() {
+            return new HDWalletProvider( [privateKey], "https://rpc.chiadochain.net")},
+      network_id: 10200,
+      gas: 500000,
+      gasPrice: 1000000000
+},
     development: {
       host: '127.0.0.1',
       port: 8545,
@@ -26,5 +34,12 @@ module.exports = {
     solc: {
       version: "^0.8.9"
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: etherScanPrivateKey,
+    gnosisscan: gnosisscanAPIKey
   }
 }
