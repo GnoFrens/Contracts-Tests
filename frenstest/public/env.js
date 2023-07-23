e91548665872d101e356e85c8debb80f7ac2d260
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-23 13:04:28 +0900

    figured out dotenv

---
+++ frenstest/public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "AIRSTACK_API_KEY": "880e5bceface4a5ba53513604f8cdc14"
+};
--- frenstest/src/App.js
+++ frenstest/src/App.js
@@ -1,11 +1,55 @@
 import './App.css';
+import React from "react";
+import env from "react-dotenv";
+import { init, useQuery } from "@airstack/airstack-react";
 
-function App() {
-  return (
-    <div className="App">
-     <button>Click me</button>
-    </div>
+const airstackAPIKey = env.AIRSTACK_API_KEY;
+
+init(airstackAPIKey);
+
+const App = () => {
+  const { data, loading, error } = useQuery(
+    `query POAPsForGnoFrens {
+      Poaps(input: {filter: {owner: {_eq: "cryptosmiff.eth"}}, blockchain: ALL}) {
+        Poap {
+          id
+          chainId
+          eventId
+          createdAtBlockNumber
+          poapEvent {
+            chainId
+            eventId
+            eventName
+            description
+            isVirtualEvent
+            id
+          }
+          owner {
+            addresses
+            domains {
+              id
+              name
+              labelName
+            }
+          }
+        }
+      }
+    }`
   );
-}
+
+  if (loading) {
+    return <div>Loading: </div>;
+  }
+  
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  <div>
+        <h1>Flow App</h1>
+        {data}
+      </div>
+  // Render your component using the data returned by the query
+};
 
 export default App;
