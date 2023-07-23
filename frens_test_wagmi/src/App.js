f94409b74632587bbb3bba2176747d82b004c52c
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-23 14:43:05 +0900

    test wagmi app

--- frens_test_wagmi/src/App.js
+++ frens_test_wagmi/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContractWrite, } from 'wagmi'
+import { abi } from "./ABI"
+import {  createConfig, configureChains } from 'wagmi'
+import { gnosisChiado } from 'wagmi/chains'
+import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc'
+ 
+const { publicClient, webSocketPublicClient } = configureChains(
+  [gnosisChiado],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: `https://rpc.chiadochain.net`,
+      }),
+    }),
+  ],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
 
 function App() {
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    address: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
+    abi: abi,
+    functionName: 'mint',
+    args: ["0x0bBC0CE97721aE44caBDe8cc34Ef05be14717c0E", 1],
+    account: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => write()}>Feed</button>
+      {isLoading && <div>Check Wallet</div>}
+      {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
     </div>
-  );
+  )
 }
 
 export default App;
